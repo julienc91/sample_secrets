0e8873882bfd68a68dc7af3081d96ac98f4ae31e
Julien Chaumont <julienc91@users.noreply.github.com>
2021-07-21 17:11:39 +0200

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -5,3 +5,6 @@ var pg_user="root";
 var pg_pass="sup3rstr0ngpass1ForGG";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+
+var GitHubToken = "81c4ef6cabcf4473bb98b28de4fb9ac606b97f62"
